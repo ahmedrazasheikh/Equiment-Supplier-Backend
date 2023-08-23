3ece692d070884d82ae55498fa44b6a8ece5d9c3
Aisha <aisha.developer.20@gmail.com>
2023-08-24 03:44:01 +0500

    Good

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ app.use(cors({
   credentials: true
 }));
 // app.use(express.json());
-const storage = multer.diskStorage({
-  destination: './uploads',
-  filename: function (req, file, cb) {
+// const storage = multer.diskStorage({
+//   destination: './uploads',
+//   filename: function (req, file, cb) {
 
-    console.log("mul-file: ", file);
-    cb(null, `${new Date().getTime()}-${file.originalname}`)
-  }
-});
-const upload = multer({ storage });
+//     console.log("mul-file: ", file);
+//     cb(null, `${new Date().getTime()}-${file.originalname}`)
+//   }
+// });
+// const upload = multer({ storage });
 app.use(express.json());
 app.get('/' , (req,res)=>{
 res.send('Ahmed Raza')
@@ -108,103 +108,103 @@ app.delete("/api/v1/customer/:id", async (req, res) => {
     });
   }
 });
-app.post('/api/v1/AddProduct', upload.any(), (req, res) => {
-  try {
+// app.post('/api/v1/AddProduct', upload.any(), (req, res) => {
+//   try {
 
 
 
-    const body = req.body;
+//     const body = req.body;
 
-    if ( // validation
-      !body.name ||
-      !body.price ||
-      !body.description
-    ) {
-      res.status(400).send({
-        message: "required parameters missing",
-      });
-      return;
-    }
+//     if ( // validation
+//       !body.name ||
+//       !body.price ||
+//       !body.description
+//     ) {
+//       res.status(400).send({
+//         message: "required parameters missing",
+//       });
+//       return;
+//     }
 
-    console.log("req.body: ", req.body);
-    console.log("req.files: ", req.files);
+//     console.log("req.body: ", req.body);
+//     console.log("req.files: ", req.files);
 
-    console.log("uploaded file name: ", req.files[0].originalname);
-    console.log("file type: ", req.files[0].mimetype);
-    console.log("file name in server folders: ", req.files[0].filename);
-    console.log("file path in server folders: ", req.files[0].path);
-
-
-    bucket.upload(
-      req.files[0].path,
-      {
-        destination: `tweetPictures/${req.files[0].filename}`, // give destination name if you want to give a certain name to file in bucket, include date to make name unique otherwise it will replace previous file with the same name
-      },
-      function (err, file, apiResponse) {
-        if (!err) {
-
-          file.getSignedUrl({
-            action: 'read',
-            expires: '03-09-2999'
-          }).then((urlData, err) => {
-            if (!err) {
-              console.log("public downloadable url: ", urlData[0]) // this is public downloadable url 
-
-              try {
-                fs.unlinkSync(req.files[0].path)
-                //file removed
-              } catch (err) {
-                console.error(err)
-              }
-
-              let addPRoduct = new tweetModel({
-                name: body.name,
-                price: body.price,
-                imageUrl: urlData[0],
-                description: body.description,
-                category: body.value
-              })
-
-              addPRoduct.save().then((res) => {
-                // res.send(res)
-
-                console.log(res, "ProDUCT ADD");
-              })
-
-              // tweetModel.create({
-              //     name : body.Name,  
-              //     price: body.Price,
-              //     imageUrl: urlData[0],
-              //     description : body.Description,
-              // },
-              //     (err, saved) => {
-              //         if (!err) {
-              //             console.log("saved: ", saved);
-
-              //             res.send({
-              //                 message: "tweet added successfully"
-              //             });
-              //         } else {
-              //             console.log("err: ", err);
-              //             res.status(500).send({
-              //                 message: "server error"
-              //             })
-              //         }
-              //     })
-            }
-          })
-        } else {
-          console.log("err: ", err)
-          res.status(500).send();
-        }
-      });
+//     console.log("uploaded file name: ", req.files[0].originalname);
+//     console.log("file type: ", req.files[0].mimetype);
+//     console.log("file name in server folders: ", req.files[0].filename);
+//     console.log("file path in server folders: ", req.files[0].path);
 
 
+//     bucket.upload(
+//       req.files[0].path,
+//       {
+//         destination: `tweetPictures/${req.files[0].filename}`, // give destination name if you want to give a certain name to file in bucket, include date to make name unique otherwise it will replace previous file with the same name
+//       },
+//       function (err, file, apiResponse) {
+//         if (!err) {
 
-  } catch (error) {
-    console.log("error: ", error);
-  }
-})
+//           file.getSignedUrl({
+//             action: 'read',
+//             expires: '03-09-2999'
+//           }).then((urlData, err) => {
+//             if (!err) {
+//               console.log("public downloadable url: ", urlData[0]) // this is public downloadable url 
+
+//               try {
+//                 fs.unlinkSync(req.files[0].path)
+//                 //file removed
+//               } catch (err) {
+//                 console.error(err)
+//               }
+
+//               let addPRoduct = new tweetModel({
+//                 name: body.name,
+//                 price: body.price,
+//                 imageUrl: urlData[0],
+//                 description: body.description,
+//                 category: body.value
+//               })
+
+//               addPRoduct.save().then((res) => {
+//                 // res.send(res)
+
+//                 console.log(res, "ProDUCT ADD");
+//               })
+
+//               // tweetModel.create({
+//               //     name : body.Name,  
+//               //     price: body.Price,
+//               //     imageUrl: urlData[0],
+//               //     description : body.Description,
+//               // },
+//               //     (err, saved) => {
+//               //         if (!err) {
+//               //             console.log("saved: ", saved);
+
+//               //             res.send({
+//               //                 message: "tweet added successfully"
+//               //             });
+//               //         } else {
+//               //             console.log("err: ", err);
+//               //             res.status(500).send({
+//               //                 message: "server error"
+//               //             })
+//               //         }
+//               //     })
+//             }
+//           })
+//         } else {
+//           console.log("err: ", err)
+//           res.status(500).send();
+//         }
+//       });
+
+
+
+//   } catch (error) {
+//     console.log("error: ", error);
+//   }
+// })
 app.post('/signup', async (req, res) => {
   try {
     const { username, email, password } = req.body;
